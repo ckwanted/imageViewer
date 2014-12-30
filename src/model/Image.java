42138b7f89b05adcb146a62b8a9bad6617bc6f64
ckwanted <ckwanted@gmail.com>
2014-12-30 14:55:36 +0000

    Mock

--- src/model/Image.java
+++ src/model/Image.java
@@ -4,6 +4,6 @@ public interface Image {
     public Bitmap getBitmap();
     public Image getNext();
     public Image getPrev();
-    public void setNext(Image image);
-    public void setPrev(Image image);
+    public void setNext(Image next);
+    public void setPrev(Image prev);
 }
